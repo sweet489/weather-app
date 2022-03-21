b85b662aabce1828be83cbbef17b7d81b2c6cfe1
admin <admin@MacBook-Pro-admin.local>
2022-03-21 18:27:37 +0300

    commit

--- pages/api/geo.js
+++ pages/api/geo.js
@@ -1,8 +1,8 @@
 import geoip from 'geoip-lite';
 
 export default (req, res) => {
-    var ip = req.ip;
-    var geo = geoip.lookup(ip);
+    let ip = req.connection.remoteAddress;
+    let geo = geoip.lookup(ip);
 
-    res.status(200).json({geo});
+    res.status(200).json({ip});
 }
